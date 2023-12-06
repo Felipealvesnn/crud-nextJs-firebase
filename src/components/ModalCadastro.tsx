6175fb63331a93d0c1999930db41eee327ec043e
Felipe Nunes Work <felipe.rudra@hotmail.com>
2023-12-06 16:52:04 -0300

    att

--- src/components/ModalCadastro.tsx
+++ src/components/ModalCadastro.tsx
@@ -23,6 +23,7 @@ export default function AlertDialog(props: ModalProps) {
             onClose();
             setNome('');
             setIdade(0);
+            props.abrirOuFecharModal();
         }
     };
 
