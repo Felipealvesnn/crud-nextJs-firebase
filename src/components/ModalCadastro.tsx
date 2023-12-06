967975dee1ab46b58c3e41e5dc4d0328dc4b2792
Felipe Nunes Work <felipe.rudra@hotmail.com>
2023-12-06 13:51:28 -0300

    att

--- src/components/ModalCadastro.tsx
+++ src/components/ModalCadastro.tsx
@@ -7,61 +7,64 @@ import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import Inputs from './Inputs';
 import { Box } from '@mui/material';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Cliente from '../core/Cliente';
 interface ModalProps {
     aberto: boolean
+    ClienteMudou?: (cliente: Cliente) => void
     abrirOuFecharModal: () => void
     cliente?: Cliente
+   
+
 
 }
 
+// ... (imports e interface ModalProps)
+// ... (imports e interface ModalProps)
 
 export default function AlertDialog(props: ModalProps) {
     const id = props.cliente?.id;
-    const [nome, setNome] = useState(props.cliente?.nome);
-    const [idade, setIdade] = useState(props.cliente?.idade);
+    const [nome, setNome] = useState(props.cliente?.nome ?? '');
+    const [idade, setIdade] = useState(props.cliente?.idade ?? 0);
+
+    // Atualizar os estados internos quando o cliente em edição for alterado
+    useEffect(() => {
+        setNome(props.cliente?.nome ?? '');
+        setIdade(props.cliente?.idade ?? 0);
+    }, [props.cliente]);
 
     function body() {
         return (
-            <Box sx={{ display: 'flex', flexWrap: 'wrap', }}>
-                {
-                    id ? <Inputs disabled tipo='number' texto='Id' valor={id} /> : null
-                }
+            <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
+                {id ? <Inputs disabled tipo='number' texto='Id' valor={id} /> : null}
                 <Inputs valorMudou={setNome} tipo='texto' texto='Nome' valor={nome} />
                 <Inputs valorMudou={setIdade} valor={idade} tipo='number' texto='Idade' />
                 <Inputs tipo='password' showPassword texto='Senha' />
             </Box>
-        )
+        );
     }
-
-
-
-
-
 
     return (
         <>
-
             <Dialog
                 open={props.aberto}
                 onClose={props.abrirOuFecharModal}
                 aria-labelledby="alert-dialog-title"
                 aria-describedby="alert-dialog-description"
             >
-                <DialogTitle id="alert-dialog-title">
-                    {"Cadastrar pessoa"}
-                </DialogTitle>
+                <DialogTitle id="alert-dialog-title">{"Cadastrar pessoa"}</DialogTitle>
                 <DialogContent>
-                    <DialogContentText id="alert-dialog-description">
-                        {body()}
-                    </DialogContentText>
+                    <DialogContentText id="alert-dialog-description">{body()}</DialogContentText>
                 </DialogContent>
                 <DialogActions>
-                    <Button onClick={props.abrirOuFecharModal}>fechar</Button>
-                    <Button onClick={() => {
-                        console.log(nome, idade)
-                    }} autoFocus>
+                    <Button onClick={props.abrirOuFecharModal}>Fechar</Button>
+                    <Button
+                        onClick={() => {
+                            props.ClienteMudou?.(new Cliente(nome, idade, id));
+                            props.abrirOuFecharModal();
+                        }}
+                        autoFocus
+                    >
                         Salvar
                     </Button>
                 </DialogActions>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,55 +1,69 @@
-import { Fragment, useRef, useState } from 'react'
-import Layout from '../components/Layout'
-import Tabela from '../components/Tabela'
-import Botao from '../components/Botao'
-import Cliente from '../core/Cliente'
-import Modal from '../components/ModalCadastro'
-
-
-
-const clientes = [
-  new Cliente('Ana', 34, '1'),
-  new Cliente('Bia', 45, '2'),
-  new Cliente('Carlos', 23, '3'),
-  new Cliente('Pedro', 54, '4'),
-]
-function clienteSelecionado(cliente: Cliente) {
-  console.log(cliente.nome)
-}
-function clienteExcluido(cliente: Cliente) {
-  console.log(cliente.idade)
-}
-
-
+import { useRef, useState } from 'react';
+import Layout from '../components/Layout';
+import Tabela from '../components/Tabela';
+import Botao from '../components/Botao';
+import Cliente from '../core/Cliente';
+import Modal from '../components/ModalCadastro';
 
 export default function Home() {
-  const [modalOpen, setModalOpen] = useState(false)
+  const [clientes, setClientes] = useState([
+    new Cliente('Ana', 34, '1'),
+    new Cliente('Bia', 45, '2'),
+    new Cliente('Carlos', 23, '3'),
+    new Cliente('Pedro', 54, '4'),
+  ]);
+
+  const [clienteEmEdicao, setClienteEmEdicao] = useState<Cliente | null>(null);
+  const [modalOpen, setModalOpen] = useState(false);
+
+  function clienteEditar(cliente: Cliente) {
+    setClienteEmEdicao(cliente);
+    abrirOuFecharModal();
+  }
+
+  function clienteExcluido(cliente: Cliente) {
+    const index = clientes.indexOf(cliente);
+    setClientes(clientes.filter((_, i) => i !== index));
+  }
+
+  function clienteSalvo(cliente: Cliente) {
+    if (clienteEmEdicao) {
+      // Se clienteEmEdicao não for nulo, significa que estamos editando um cliente existente
+      const index = clientes.indexOf(clienteEmEdicao);
+      const novosClientes = [...clientes];
+      novosClientes[index] = cliente;
+      setClientes(novosClientes);
+      setClienteEmEdicao(null); // Limpar o cliente em edição após salvar
+    } else {
+      // Caso contrário, estamos adicionando um novo cliente
+      setClientes([...clientes, cliente]);
+    }
+  }
 
   const abrirOuFecharModal = () => {
     setModalOpen(!modalOpen);
   };
 
-
-
-
   return (
     <>
       <div className='flex items-center justify-center min-h-screen py-2 bg-slate-500 '>
-
         <Layout titulo='Cadastro Simples'>
           <Botao cor="gray" acao={abrirOuFecharModal}>
             Novo cliente
           </Botao>
-          <Tabela clientes={clientes}
+          <Tabela
+            clientes={clientes}
             clienteExcluido={clienteExcluido}
-            clienteSelecionado={clienteSelecionado} />
-
+            clienteSelecionado={clienteEditar}
+          />
         </Layout>
-
-        <Modal aberto={modalOpen} abrirOuFecharModal={abrirOuFecharModal} ></Modal>
+        <Modal
+          ClienteMudou={clienteSalvo}
+          aberto={modalOpen}
+          abrirOuFecharModal={abrirOuFecharModal}
+          cliente={clienteEmEdicao as Cliente} // Passar o cliente em edição para o modal
+        ></Modal>
       </div>
-
     </>
-
-  )
+  );
 }
