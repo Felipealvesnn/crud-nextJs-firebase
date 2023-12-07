14c7765a73fd8e414981c2a64e3d46c9bac0d3bf
Felipe Nunes Work <felipe.rudra@hotmail.com>
2023-12-07 11:45:29 -0300

    att

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
-import { useRef, useState } from 'react';
+// ...
+
+import { useEffect, useState } from 'react';
 import Layout from '../components/Layout';
 import Tabela from '../components/Tabela';
 import Botao from '../components/Botao';
@@ -8,69 +10,77 @@ import ClienteRepository from '../core/ClienteRepository';
 import ColecaoCliente from '../backend/db/ColecaoCliente';
 
 export default function Home() {
-  const repo: ClienteRepository = new ColecaoCliente()
+  const repo: ClienteRepository = new ColecaoCliente();
 
-  const [clientes, setClientes] = useState([
-    new Cliente('Ana', 34, '1'),
-    new Cliente('Bia', 45, '2'),
-    new Cliente('Carlos', 23, '3'),
-    new Cliente('Pedro', 54, '4'),
-  ]);
-
+  const [clientes, setClientes] = useState<Cliente[]>([]);
   const [clienteEmEdicao, setClienteEmEdicao] = useState<Cliente>(Cliente.vazio());
   const [modalOpen, setModalOpen] = useState(false);
+
+  useEffect(() => {
+    repo.getAll().then((clientes) => {
+      setClientes(clientes);
+    });
+  }, [repo]);
+
+  const abrirOuFecharModal = () => {
+    setModalOpen(!modalOpen);
+  };
 
   function clienteEditar(cliente: Cliente) {
     setClienteEmEdicao(cliente);
     abrirOuFecharModal();
   }
 
-  function clienteExcluido(cliente: Cliente) {
-    const index = clientes.indexOf(cliente);
-    setClientes(clientes.filter((_, i) => i !== index));
-  }
-
-  function clienteSalvo(cliente: Cliente) {
-    if (clienteEmEdicao) {
-      // Se clienteEmEdicao não for nulo, significa que estamos editando um cliente existente
-      const index = clientes.indexOf(clienteEmEdicao);
-      const novosClientes = [...clientes];
-      novosClientes[index] = cliente;
-      setClientes(novosClientes);
-      setClienteEmEdicao(Cliente.vazio()); // Limpar o cliente em edição após salvar
-    } else {
-      // Caso contrário, estamos adicionando um novo cliente
-      setClientes([...clientes, cliente]);
+  async function clienteExcluido(cliente: Cliente) {
+    try {
+      await repo.excluir(cliente);
+      setClientes(clientes.filter((c) => c.id !== cliente.id));
+    } catch (error) {
+      console.error('Erro ao excluir cliente:', error);
     }
   }
 
-  const abrirOuFecharModal = () => {
-    setModalOpen(!modalOpen);
+  async function clienteSalvo(cliente: Cliente) {
+    try {
+      await repo.salvar(cliente);
+      setClientes((prevClientes) => {
+        const index = prevClientes.findIndex((c) => c.id === cliente.id);
+        if (index !== -1) {
+          // Se o cliente já existe, atualize-o
+          const newClientes = [...prevClientes];
+          newClientes[index] = cliente;
+          return newClientes;
+        } else {
+          // Se o cliente não existe, adicione-o
+          return [...prevClientes, cliente];
+        }
+      });
 
-
-  };
+      setClienteEmEdicao(Cliente.vazio()); // Limpar o cliente em edição após salvar
+    } catch (error) {
+      console.error('Erro ao salvar cliente:', error);
+    }
+  }
 
   return (
-    <>
-      <div className='flex items-center justify-center min-h-screen py-2 bg-slate-500 '>
-        <Layout titulo='Cadastro Simples'>
-          <Botao cor="gray" acao={abrirOuFecharModal}>
-            Novo cliente
-          </Botao>
-          <Tabela
-            clientes={clientes}
-            clienteExcluido={clienteExcluido}
-            clienteSelecionado={clienteEditar}
-          />
-        </Layout>
-        <Modal
-          onClose={() => setClienteEmEdicao(Cliente.vazio())}
-          ClienteMudou={clienteSalvo}
-          aberto={modalOpen}
-          abrirOuFecharModal={abrirOuFecharModal}
-          cliente={clienteEmEdicao as Cliente} // Passar o cliente em edição para o modal
-        ></Modal>
-      </div>
-    </>
+    <div className='flex items-center justify-center min-h-screen py-2 bg-slate-500 '>
+      <Layout titulo='Cadastro Simples'>
+        <Botao cor="gray" acao={abrirOuFecharModal}>
+          Novo cliente
+        </Botao>
+        <Tabela
+          clientes={clientes}
+          clienteExcluido={clienteExcluido}
+          clienteSelecionado={clienteEditar}
+        />
+      </Layout>
+      <Modal
+        onClose={() => setClienteEmEdicao(Cliente.vazio())}
+        ClienteMudou={clienteSalvo}
+        aberto={modalOpen}
+        abrirOuFecharModal={abrirOuFecharModal}
+        cliente={clienteEmEdicao} // Passar o cliente em edição para o modal
+      ></Modal>
+    </div>
   );
 }
