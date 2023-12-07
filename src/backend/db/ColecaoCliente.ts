bb35deaf16aad9491f840f812cf7749038093c93
Felipe Nunes Work <felipe.rudra@hotmail.com>
2023-12-07 10:35:49 -0300

    aii

--- src/backend/db/ColecaoCliente.ts
+++ src/backend/db/ColecaoCliente.ts
@@ -1,34 +1,61 @@
-import Cliente from '@/src/core/Cliente'
-import ClienteRepository from '../../core/ClienteRepository'
-import firebase from '../config'
-
+import Cliente from '@/src/core/Cliente';
+import ClienteRepository from '../../core/ClienteRepository';
+import { collection, deleteDoc, doc, getDocs, setDoc, updateDoc } from 'firebase/firestore';
+import db from '../config';
 
 export default class ColecaoCliente implements ClienteRepository {
-    #conversor = {
-        toFirestore(cliente: Cliente) {
-            return {
-                nome: cliente.nome,
-                idade: cliente.idade,
-                id: cliente.id
-            }
-        },
-        // recebe algo do firestore e converte pra classe 
-        fromFirestore: (snapshot:any, options:any) => {
-            const data = snapshot.data(options);
-            return new Cliente(data.nome, data.idade, data.id);
-        }
+  #conversor = {
+    toFirestore(cliente: Cliente) {
+      return {
+        nome: cliente.nome,
+        idade: cliente.idade,
+        id: cliente.id,
+      };
+    },
+    fromFirestore: (snapshot: any, options: any) => {
+      const data = snapshot.data(options);
+      return new Cliente(data.nome, data.idade, data.id);
+    },
+  };
+
+  async excluir(cliente: Cliente): Promise<void> {
+    await deleteDoc(doc(db, 'Clientes', cliente.id));
+  }
+
+  async getAll(): Promise<Cliente[]> {
+    const querySnapshot = await getDocs(collection(db, 'Clientes').withConverter(this.#conversor));
+    const clientes: Cliente[] = [];
+    
+    querySnapshot.forEach((doc) => {
+      clientes.push(doc.data());
+    });
+
+    return clientes;
+  }
+
+  async salvar(cliente: Cliente): Promise<Cliente> {
+    const clientesRef = collection(db, 'Clientes').withConverter(this.#conversor);
+    
+    if (cliente?.id) {
+      // Verificar se o cliente já existe
+      const docSnapshot = await doc(clientesRef, cliente.id);
+      
+      if (docSnapshot.id) {
+        // Atualizar o cliente se ele já existir
+        await updateDoc(doc(clientesRef, cliente.id), {
+          nome: cliente.nome,
+          idade: cliente.idade,
+        });
+      } else {
+        // Adicionar um novo cliente
+        await setDoc(doc(clientesRef, cliente.id), {
+          nome: cliente.nome,
+          idade: cliente.idade,
+          id: cliente.id,
+        });
+      }
     }
 
-    excluir(cliente: Cliente): Promise<void> {
-        throw new Error('Method not implemented.')
-    }
-    getAll(): Promise<Cliente[]> {
-        throw new Error('Method not implemented.')
-    }
-    async salvar(cliente: Cliente): Promise<Cliente> {
-        return Cliente.vazio()
-    }
-
-
-
+    return cliente;
+  }
 }
